fbd5a2eec7a0466ac377c92efd60195da8f39599
wem <wem@kth.se>
2022-02-08 20:11:13 +0100

    Seperation of concern

--- Rollout/main.py
+++ Rollout/main.py
@@ -9,144 +9,11 @@ from natsort import natsorted
 
 # Credit for this: Nicholas Swift
 # as found at https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
-from warnings import warn
-import heapq
+
 import random
 import time
+from A_star import astar
 
-
-class Node:
-    """
-    A node class for A* Pathfinding
-    """
-
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-
-    def __repr__(self):
-        return f"{self.position} - g: {self.g} h: {self.h} f: {self.f}"
-
-    # defining less than for purposes of heap queue
-    def __lt__(self, other):
-        return self.f < other.f
-
-    # defining greater than for purposes of heap queue
-    def __gt__(self, other):
-        return self.f > other.f
-
-
-def return_path(current_node):
-    path = []
-    current = current_node
-    while current is not None:
-        path.append(current.position)
-        current = current.parent
-    return path[::-1]  # Return reversed path
-
-
-def astar(maze, start, end, allow_diagonal_movement=False):
-    """
-    Returns a list of tuples as a path from the given start to the given end in the given maze
-    :param maze:
-    :param start:
-    :param end:
-    :return:
-    """
-
-    # Create start and end node
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
-
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
-
-    # Heapify the open_list and Add the start node
-    heapq.heapify(open_list)
-    heapq.heappush(open_list, start_node)
-
-    # Adding a stop condition
-    outer_iterations = 0
-    max_iterations = (10*len(maze[0]) * len(maze) // 2)
-
-    # what squares do we search
-    adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
-    if allow_diagonal_movement:
-        adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),
-                            (-1, -1), (-1, 1), (1, -1), (1, 1),)
-
-    # Loop until you find the end
-    while len(open_list) > 0:
-        outer_iterations += 1
-
-        if outer_iterations > max_iterations:
-            # if we hit this point return the path such as it is
-            # it will not contain the destination
-            warn("giving up on pathfinding too many iterations")
-            return return_path(current_node)
-
-        # Get the current node
-        current_node = heapq.heappop(open_list)
-        closed_list.append(current_node)
-
-        # Found the goal
-        if current_node == end_node:
-            return return_path(current_node)
-
-        # Generate children
-        children = []
-
-        for new_position in adjacent_squares:  # Adjacent squares
-
-            # Get node position
-            node_position = (
-                current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) - 1) or node_position[1] < 0:
-                continue
-
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
-
-            # Create new node
-            new_node = Node(current_node, node_position)
-
-            # Append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-            # Child is on the closed list
-            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
-                continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = abs(child.position[0] - end_node.position[0]) + \
-                abs(child.position[1] - end_node.position[1])
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            if len([open_node for open_node in open_list if child.position == open_node.position and child.g > open_node.g]) > 0:
-                continue
-
-            # Add the child to the open list
-            heapq.heappush(open_list, child)
-
-    warn("Couldn't get a path to destination")
-    return None
 
 
 def pre_processing(state_mat, target):
@@ -193,14 +60,13 @@ def base_policy(state, return_list, agent_number):
     if len(actions) == 0:
         return [0] if return_list else 0
     return actions if return_list else actions[0]
-
-
 def action_picker(env, prev_actions, state, num_of_agents, depth, num_of_steps, prev_pass_actions, base_policy_actions):
     action_space = 5
     R = [0]*action_space
     pre_pick_time = 0
     A_star_time = 0
     sim_time = 0 
+    action_picker_time = 0 
     for action in range(action_space):  # For every action
         pre_pick_start = time.time()
         new_state = copy.deepcopy(state)
@@ -229,28 +95,38 @@ def action_picker(env, prev_actions, state, num_of_agents, depth, num_of_steps, 
         path = [0]*num_of_agents
 
         start_sim = time.time()
+        step_time = 0
         while n < depth:
+
+            step_start = time.time()
             curr_state, reward, done, info = env.step(action_list, "raw")
+            step_time = time.time() - step_start
             R[action] += reward
             if done:
                 break
             action_list = [0]*num_of_agents
+            action_picker_start = time.time()
             for i in range(num_of_agents):
                 if targets[i] != curr_state[1][i] or len(path[i]) == 0:
                     start_A_star = time.time()
                     path[i] = base_policy(
                         copy.deepcopy(curr_state), True, i)
+                    #print(f"Base policy call at n={n} for agent={i}")
                     targets[i] = curr_state[1][i]
                     end_A_star = time.time()
                     A_star_time += (end_A_star - start_A_star)
                     #print(end_A_star - start_A_star)
                 action_list[i] = path[i].pop(0)
+            action_picker_time += (time.time() - action_picker_start)
             n += 1
         end_sim = time.time()
         sim_time += (end_sim - start_sim)
     
+    print(f"Step time: {step_time}")
     #print("Pre-pick time "+ str(pre_pick_time))
-    #print("Sim time: "+ str(sim_time))
+    print("Sim time: "+ str(sim_time))
+    print(f"Action picker: {action_picker_time}")
+    print(f"A-star: {A_star_time}")
     #print("Sim time without A*: " + str(sim_time - A_star_time))
     #print("Percent A-star "+str(100*A_star_time/sim_time)+"%")
     #print("Percent pre-pick "+str(100*pre_pick_time/(sim_time+pre_pick_time))+"%")
