1df95fc6963e657ba9561326c9b899d054eaef43
William Emanuelsson <wem@kth.se>
2022-03-13 19:49:54 +0100

    Another bug fix

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -2,13 +2,13 @@
 #include "Environment.hpp"
 
 int main(){
-    Environment env(3, 2, 5, 2);
+    Environment env(1, 2, 2, 2);
     env.printMatrix();
 
     std::vector<uint8_t> actions = {4, 2};
 
     std::pair<unsigned int, unsigned int> target1(0, 0);
-    std::pair<unsigned int, unsigned int> target2(3, 3);
+    std::pair<unsigned int, unsigned int> target2(2, 2);
     std::vector<std::pair<unsigned int, unsigned int>> targets = {target1, target2};
 
     std::cout << env.step(actions, targets) << std::endl;
