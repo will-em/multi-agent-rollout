5c96ee8a4967c6b0d423e180b62567dbf8a7b627
William Emanuelsson <wem@kth.se>
2022-03-13 22:05:27 +0100

    Implement discount factor

--- CPP/Environment.cpp
+++ CPP/Environment.cpp
@@ -137,14 +137,12 @@ double Environment::step(std::vector<uint8_t> actions, std::vector<std::pair<uns
     for(size_t i = 0; i < m_agentPositions.size(); ++i){
         std::vector<unsigned int> oldToNew = {m_agentPositions[i].first, m_agentPositions[i].second, newPositions[i].first, newPositions[i].second};
         for(size_t j = i + 1; j < m_agentPositions.size(); ++j){
-            if(newPositions[i] == newPositions[j]){ // If two agents have the same position
-                cost += c_collision;
-            }
             std::vector<unsigned int> complement = {newPositions[j].first, newPositions[j].second, m_agentPositions[j].first, m_agentPositions[j].second};
-
-            if(oldToNew == complement){ // If to agents swap positions
-                cost += c_collision;
+            // If two agents have the same position or swap positions
+            if(newPositions[i] == newPositions[j] || oldToNew == complement){ 
+                cost += c_collision * discountFactors.arr[m_stepCount];
             }
+
         }
     }
 
