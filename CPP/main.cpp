8709b2ae3f63f5e38633a192cf16b0ad335142f6
William Emanuelsson <wem@kth.se>
2022-03-30 02:22:55 +0200

    Add helper functions

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -2,20 +2,69 @@
 #include "Environment.hpp"
 #include <math.h>   
 
-bool isDone(Environment &env){
-    /*
-        TODO:
-        Check if all matrix elements at the agentsPositions are non-negative
-    */
-    return false;
+std::pair indexToPair(int i, dim){
+    // Convert flattened index to 2d coordinate
+    int x = (int) floor(i / dim);
+    int y = i % dim; 
+
+    std::pair target = {x, y};
+    return target;
+}
+
+void targetPicker(Environment &env, std::vector< std::pair<int,int> > &targets, int agentIdx){
+    const int dim = env.getDim();
+    int* matrix = env.getMatPtr();
+
+    if(targets.empty()){ // Initialization of targets
+        int targetsAdded = 0;
+        for(size_t i = 0; i < (dim * dim); ++i){
+            if(matrix[i] == 2){ // If it is a box
+                targets.push_back(indexToPair(i, dim));
+                targetsAdded++; 
+                if(targetsAdded == env.getNumOfAgents())
+                    break;
+            }
+        }
+    }
+    else{
+        for(size_t i = 0; i < (dim * dim); ++i){
+            if(matrix[i] == 2){
+                // Check that this box is not the target of another agent
+                auto boxPos = indexToPair(i, dim);
+                for(auto target : targets){
+                    if(boxPos == target)
+                        continue;
+                }
+                targets.push_back(boxPos);
+            }
+        }        
+    }
+}
+
+std::vector<int> basePolicy(Environment &env, int agentIdx){
+    
+}
+
+std::vector<int> actionPicker(Environment &env){
+
+    int numOfAgents = env.getNumOfAgents(); 
+
+
+    // Get base policies for numOfAgents - 1 agents 
+    std::vector< std::vector<int> > basePolicies(numOfAgents - 1);
+    for(size_t i = 1; i < numOfAgents; ++i){
+        basePolicies.push_back(basePolicy(env, i));
+    } 
 }
 
 void simulate(int numOfAgents){ 
+    int wallOffset = 1;
+    int boxOffset = 1;
     int n = (int)ceil(sqrt(numOfAgents));
-    Environment env(1, 1, n, numOfAgents);
+    Environment env(wallOffset, boxOffset, n, numOfAgents);
 
-    while(!isDone(env)){
-        
+    while(!env.isDone()){
+
     }
 }
 
