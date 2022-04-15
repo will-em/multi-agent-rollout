3f756c160a7b97fe418984985a7ec05f33c5eab3
William Emanuelsson <wem@kth.se>
2022-04-15 23:58:39 +0200

    Remove comment

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -376,51 +376,6 @@ void simulate(int numOfAgents){
 }
 
 int main(){
-    /*
-    Environment env(3, 3, 3, 2);
-    env.printMatrix();
-
-    std::vector<uint8_t> actions = {4, 4};
-
-    std::pair<unsigned int, unsigned int> target1(0, 0);
-    std::pair<unsigned int, unsigned int> target2(2, 2);
-    std::vector<std::pair<unsigned int, unsigned int>> targets = {target1, target2};
-    std::cout << env.step(actions, targets) << std::endl;
-    actions[1] = 3;
-
-    env.printMatrix();
-    targets[1] = {2, 3};
-    std::cout << env.step(actions, targets) << std::endl;
-    env.printMatrix();
-    std::pair<unsigned int, unsigned int> target(150, 150);
-    int N = 100;
-    std::vector<std::pair<unsigned int, unsigned int>> targets(N, target);
-    std::vector<uint8_t> actions(N, 2);
-
-    for(int i=0; i<N*5; i++){
-        Environment env(N/2, 1, 1, N);
-        for(int j=0; j<100; j++){
-            env.step(actions, targets);
-        }
-    }
-    */
-   /*
-    Environment env(5, 1, 1, 2);
-    env.printMatrix();
-
-    std::vector<int> actions = {1, 4};
-
-    std::pair<int, int> target1(0, 0);
-    std::pair<int, int> target2(2, 2);
-    std::vector<std::pair<int, int>> targets = {target1, target2};
-    std::cout << env.step(actions, targets) << std::endl;
-    env.printMatrix(); 
-
-    std::cout << env.step(actions, targets) << std::endl;
-    env.printMatrix(); 
-    */
-
-
     simulate(3);
 
     return 0;
