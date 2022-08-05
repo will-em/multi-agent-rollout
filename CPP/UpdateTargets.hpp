ee29c441e42c1d54dbff7365f2ce00c8c54abef4
William Emanuelsson <wem@kth.se>
2022-08-05 22:15:03 +0200

    Move updateTargets function into separate file

---
+++ CPP/UpdateTargets.hpp
@@ -0,0 +1,15 @@
+# pragma once
+#include "Environment.hpp"
+#include <vector>
+
+/**
+    Updates the target of agents that have picked up or dropped of a box. 
+
+    @param env Reference to an environment 
+    @param targets Reference to a vector of agent positions
+    @param beforeValues Reference to a vector of matrix elements before executing the set of given controls
+    @param afterValues Reference to a vector of matrix elements after executing the set of given controls 
+    @returns A boolean vector that, for each agent, tells you whether or not the agent has updated its target
+
+*/
+std::vector<bool> updateTargets(Environment &env, std::vector<std::pair<int, int>> &targets, std::vector<int> &beforeValues, std::vector<std::pair<int, int>> dropOffPoints, int seed);
--- CPP/main.cpp
+++ CPP/main.cpp
@@ -9,29 +9,9 @@
 #include "BasePolicy.hpp"
 #include "CostsToControl.hpp"
 #include "BoxPicker.hpp"
+#include "UpdateTargets.hpp"
 #include <random>
 
-std::vector<bool> updateTargets(Environment &env, std::vector<std::pair<int, int>> &targets, std::vector<int> &beforeValues, std::vector<std::pair<int, int>> dropOffPoints, int seed){
-    std::vector<int> afterValues = env.getAgentValues();
-    int numOfAgents = env.getNumOfAgents();
-
-    std::vector<bool> hasUpdatedTarget(numOfAgents, false);
-
-    srand(seed);
-    // Update targets if necessary 
-    for(size_t i = 0; i < numOfAgents; ++i){
-        if(afterValues[i] > beforeValues[i]){ // Agent i picks up box
-            targets[i] = dropOffPoints[rand() % dropOffPoints.size()];
-            hasUpdatedTarget[i] = true;
-        }
-        else if(afterValues[i] < beforeValues[i]){ // Agent i drops off box
-            boxPicker(env, targets, i);
-            hasUpdatedTarget[i] = true;
-        }
-    }
-
-    return hasUpdatedTarget; // C++11 move
-}
 void updateBasePolicy(Environment &env, std::vector<std::pair<int, int>> &targets, std::vector<bool> &hasUpdatedTarget, std::vector<std::vector<int>> &basePolicies){
     int numOfAgents = env.getNumOfAgents();
 
