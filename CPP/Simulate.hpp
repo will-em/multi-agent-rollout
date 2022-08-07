cdeb7985f371e33c6a45746a0f6a1817b5415cb6
William Emanuelsson <wem@kth.se>
2022-08-07 16:52:18 +0200

    Update function information comment

--- CPP/Simulate.hpp
+++ CPP/Simulate.hpp
@@ -1,12 +1,8 @@
 //#pragma once
 /**
-    Updates the target of agents that have picked up or dropped of a box 
-
-    @param env Reference to an environment 
-    @param targets Reference to a vector of agent positions
-    @param beforeValues Reference to a vector of matrix elements before executing the set of given controls
-    @param afterValues Reference to a vector of matrix elements after executing the set of given controls 
-    @returns A boolean vector that, for each agent, tells you whether or not the agent has updated its target
+    Simulate the multi-agent warehouse
+    @param numOfAgents Number of agents to be simulated
+    @returns Boolean indicating whether the agents completed the task without collision 
 
 */
 bool simulate(int numOfAgents);
