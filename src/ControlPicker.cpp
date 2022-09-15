d79df2e8ee950a271dc1665f377395f0ab8e9eed
William Emanuelsson <wem@kth.se>
2022-09-15 16:54:30 +0200

    Simplify code

--- src/ControlPicker.cpp
+++ src/ControlPicker.cpp
@@ -32,87 +32,62 @@ std::vector<int> controlPicker(Environment &env, std::vector<std::pair<int, int>
     for(int agentIdx : agentOrder){
         std::vector<double> costs(5);
         for(size_t c0 = 0; c0 < 5; ++c0){
-            std::vector<double> subTreeCosts;
-            //for(size_t c1 = 0; c1 < 5; ++c1){
-                //for(size_t c2 = 0; c2 < 5; ++c2){
-                    std::vector<std::pair<int, int>> simTargets(targets);
-                    std::vector<int> controls(numOfAgents);
+            std::vector<std::pair<int, int>> simTargets(targets);
+            std::vector<int> controls(numOfAgents);
 
-                    std::vector<std::vector<int>> basePolicies(preComputedBasePolicies);
+            std::vector<std::vector<int>> basePolicies(preComputedBasePolicies);
 
-                    for(size_t i = 0; i < optimizedControls.size(); ++i){
-                        auto basePolicy = basePolicies[agentOrder[i]];
-                        int n = basePolicy.size();
-                        if(n == 0 || basePolicy[n - 1] != optimizedControls[i]){
-                            controls[agentOrder[i]] = optimizedControls[i];
-                            basePolicies[agentOrder[i]].clear();
-                            test_n++;
-                        }
-                    }
+            for(size_t i = 0; i < optimizedControls.size(); ++i){
+                auto basePolicy = basePolicies[agentOrder[i]];
 
-                    basePolicies[agentIdx].clear();
-                    //basePolicies[agentIdx].push_back(c2);
-                    //basePolicies[agentIdx].push_back(c1);
-                    basePolicies[agentIdx].push_back(c0);
-
-                    Environment simEnv = env; // Copy environment
-                    
-                    double cost = 0.0;
-                    int iteration = 0; 
-
-                    std::vector<int> agentIdxBasePolicy;
-
-                    while(!simEnv.isDone() && iteration < 1000){
-
-                        for(size_t i = 0; i < numOfAgents; ++i){
-                            int n = basePolicies[i].size();
-
-                            if(n > 0){
-                                controls[i] = basePolicies[i][n - 1];
-                                basePolicies[i].pop_back();
-                            }
-                        }
-
-                        auto beforeValues = simEnv.getAgentValues();
-
-                        cost += simEnv.step(controls, simTargets); 
-
-                        if(cost > 1000.0){
-                            break;
-                        }
-
-                        // Update targets
-                        auto hasUpdatedTarget = updateTargets(simEnv, simTargets, beforeValues, dropOffPoints);
-
-                        //if(iteration == 0)
-                            //hasUpdatedTarget[agentIdx] = false;
-
-                        updateBasePolicy(simEnv, simTargets, hasUpdatedTarget, basePolicies);
-
-                        iteration++;
-                    }
-                    subTreeCosts.push_back(cost);
-                //}
-            //}
-            /*
-            double lowestSubTreeCost = subTreeCosts[0];
-            for(auto el : subTreeCosts){
-                if(el < lowestSubTreeCost) {
-                    lowestSubTreeCost = el;
+                if(basePolicy.size() == 0 || basePolicy.back() != optimizedControls[i]){
+                    controls[agentOrder[i]] = optimizedControls[i];
+                    basePolicies[agentOrder[i]].clear();
+                    test_n++;
                 }
-                std::cout << el << " ";
             }
-            std::cout << '\n';
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            std::cout
-            */
-            costs[c0] = *std::min_element(subTreeCosts.begin(), subTreeCosts.end()) ;
+
+            basePolicies[agentIdx].clear();
+            //basePolicies[agentIdx].push_back(c2);
+            //basePolicies[agentIdx].push_back(c1);
+            basePolicies[agentIdx].push_back(c0);
+
+            Environment simEnv = env; // Copy environment
+            
+            double cost = 0.0;
+            int iteration = 0; 
+
+            std::vector<int> agentIdxBasePolicy;
+
+            while(!simEnv.isDone() && iteration < 100){
+
+                for(size_t i = 0; i < numOfAgents; ++i){
+                    if(basePolicies[i].size() > 0){
+                        controls[i] = basePolicies[i].back();
+                        basePolicies[i].pop_back();
+                    }
+                }
+
+                auto beforeValues = simEnv.getAgentValues();
+
+                cost += simEnv.step(controls, simTargets); 
+
+                if(cost > 1000.0){
+                    break;
+                }
+
+                // Update targets
+                auto hasUpdatedTarget = updateTargets(simEnv, simTargets, beforeValues, dropOffPoints);
+
+                if(iteration == 0)
+                    hasUpdatedTarget[agentIdx] = false;
+
+                updateBasePolicy(simEnv, simTargets, hasUpdatedTarget, basePolicies);
+
+                iteration++;
+            }
+            costs[c0] = cost;
         }
-        /*
-        for(auto el : costs)
-            std::cout << el << " ";
-        std::cout << '\n';
-        */
         optimizedControls.push_back(costsToControl(costs, agentIdx, targets, env));
     }
 
